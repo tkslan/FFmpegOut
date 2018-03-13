c0fb43d72f101c4d3487eb62f7c6e564b2461ba6
tomi <ts@enkod.pl>
2018-03-13 20:18:56 +0100

    U2018 360 recording

---
+++ Assets/Render360/Scripts/Camera360.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+public class Camera360 : MonoBehaviour {
+
+    public RenderTexture leftEye;
+    public RenderTexture rightEye;
+    public RenderTexture equirect;
+    public bool renderStereo = true;
+    public float stereoSeparation = 0.064f;
+
+    void LateUpdate()
+    {
+        Camera cam = GetComponent<Camera>();
+
+        if (cam == null)
+        {
+            cam = GetComponentInParent<Camera>();
+        }
+
+        if (cam == null)
+        {
+            Debug.Log("stereo 360 capture node has no camera or parent camera");
+        }
+
+        if (renderStereo)
+        {
+            cam.stereoSeparation = stereoSeparation;
+            cam.RenderToCubemap(leftEye, 63, Camera.MonoOrStereoscopicEye.Left);
+            cam.RenderToCubemap(rightEye, 63, Camera.MonoOrStereoscopicEye.Right);
+        }
+        else
+        {
+            cam.RenderToCubemap(leftEye, 63, Camera.MonoOrStereoscopicEye.Mono);
+        }
+
+        //optional: convert cubemaps to equirect
+        leftEye.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Left);
+
+        rightEye.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Right);
+
+        if (equirect == null)
+            return;
+    }
+}
+
